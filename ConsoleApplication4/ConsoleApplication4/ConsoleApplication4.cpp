f0275abffcb4fb79daf1cf4afbc074cd48b5e4be
H0vak <novak.anton2k8@gmail.com>
2026-02-27 19:46:13 +0200

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n;
+    cout << "Введіть n: ";
+    cin >> n;
+
+    int d = n % 10;
+    int tens = (n / 10) % 10;
+    int hund = n / 100;
+
+    int m;
+    for (int x = hund; x <= 9; x++) {
+        m = x * 111;
+        if (m >= n) break;
+    }
+
+    cout << "Результат: " << m;
+    return 0;
+}
